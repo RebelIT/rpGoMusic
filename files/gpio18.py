f829cf133435e6040d9078642b270c01a9ac17c2
RebelIT <31414479+RebelIT@users.noreply.github.com>
2019-04-01 23:15:57 -0500

    gpio reader

--- files/gpio18.py
+++ files/gpio18.py
@@ -10,5 +10,5 @@ while True:
     input_state = GPIO.input(18)
     if input_state == False:
         print('Start Playing Music')
-        subprocess.Popen(["/home/pi/player"])
+        subprocess.Popen(["/etc/apps/player"])
         time.sleep(0.2)
